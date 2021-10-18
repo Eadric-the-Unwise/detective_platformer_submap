794e1c16ad54611fab2533534cefd9a8065a45bf
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2021-10-18 17:48:19 +0900

    initial commit

---
+++ src/main.c
@@ -0,0 +1,139 @@
+#include <gb/gb.h>
+#include <gb/metasprites.h>
+
+// uncomment the line below to switch on debug features
+//#define DEBUG
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+// SUBMAP-2 BRANCH PLATFORMER
+//  Include your scene and map header files
+#include "../res/tiles/brick_wide_map.h"
+#include "../res/tiles/brick_wide_tiles.h"
+#include "../res/tiles/collision_wide_map.h"
+#include "macros.h"
+#include "scene.h"
+
+UINT8 joy, last_joy;
+
+UINT8 floorYposition;
+
+const unsigned char blankmap[2] = {0x00, 0x01};
+extern Variables bkg;
+
+// CHECKS WHETHER OR NOT THE OFFSET OF PLAYER POSITION COLLIDES WITH A COLLISION TILE
+// BOTTOM LEFT PIXEL
+// UBYTE checkcollisionBL(UINT8 newplayerx, UINT8 newplayery) {
+//     UINT16 indexBLx, indexBLy, indexCamX, tileindexBL;
+//     UBYTE result;
+
+//     indexBLx = (newplayerx - 17) / 8;
+//     indexBLy = (newplayery - 1) / 8;
+//     indexCamX = (bkg.camera_x) / 8;
+//     tileindexBL = 40 * (indexBLy) + (indexBLx + indexCamX);
+
+//     result = COLLISION_WIDE_MAP[tileindexBL] == blankmap[1];
+
+//     return result;
+// }
+
+/******************************/
+// Define your OBJ and BGP palettes, show SPRITES, turn on DISPLAY
+/******************************/
+void main() {
+    DISPLAY_OFF;
+    BGP_REG = 0xE4;
+    OBP0_REG = 0xE4;
+    OBP1_REG = 0xE1;
+    SPRITES_8x16;
+    SHOW_BKG;
+    SHOW_SPRITES;
+
+    // game assumes floor is at the level of 100px. that is a temporary workaround, should use collision maps instead
+    floorYposition = 100;
+
+    init_submap();
+
+    // switch on display after everything is ready
+    DISPLAY_ON;
+    last_joy = joy = 0;
+    while (TRUE) {  // main loop runs at 60fps
+        // ---------------------------------------------
+        // process joystic input
+        last_joy = joy;
+        joy = joypad();
+
+        if (joy & J_LEFT) {
+            if (bkg.camera_style == horizontal_cam) {
+                if (bkg.camera_x) {
+                    bkg.camera_x--;
+                    bkg.redraw = TRUE;
+                }
+            }
+            if (PLAYER.SpdX > -MAX_WALK_SPEED) {
+                PLAYER.SpdX -= WALK_VELOCITY;
+            } else
+                PLAYER.SpdX = -MAX_WALK_SPEED;
+
+        } else if (joy & J_RIGHT) {
+            if (bkg.camera_style == horizontal_cam) {
+                if (bkg.camera_x < bkg.camera_max_x) {
+                    bkg.camera_x++;
+                    bkg.redraw = TRUE;
+                }
+            }
+
+            if (PLAYER.SpdX < MAX_WALK_SPEED) {
+                PLAYER.SpdX += WALK_VELOCITY;
+            } else
+                PLAYER.SpdX = MAX_WALK_SPEED;
+        }
+
+        // else if ((CHANGED_BUTTONS & J_B) && (joy & J_B) && (Jump)) {
+        // }
+
+        // #ifdef DEBUG
+        // DEBUG DETECTIVE Y COORDS
+        if (joy & J_B) {
+            printf("CamX=%u\n", bkg.camera_x);
+        }
+        // #endif
+
+        // Load stages on button press
+        // if (joy & J_START) {
+        // } else if (joy & J_SELECT) {
+        // }
+        // no joy checking below this line
+        // ---------------------------------------------
+
+        // ---------------------------------------------
+        // WORLD PHYSICS:
+        // GRAVITY
+        PLAYER.SpdY += GRAVITY;
+        if (PLAYER.SpdY > MAX_FALL_SPEED) {
+            PLAYER.SpdY = MAX_FALL_SPEED;
+        }
+        if (PLAYER.SpdX != 0) {
+            if (PLAYER.SpdX < 0)
+                PLAYER.SpdX += FRICTION;
+            else
+                PLAYER.SpdX -= FRICTION;
+        }
+
+        // update PLAYER absolute posiiton
+        PLAYER.y += PLAYER.SpdY;
+        PLAYER.x += PLAYER.SpdX;
+
+        // call level animation hook (if any), that makes other actors move (and interact in future)
+        if (animate_level) animate_level();
+
+        // render all actors on screen
+        render_actors();
+        if (bkg.redraw) {
+            wait_vbl_done();
+            set_camera();
+            bkg.redraw = FALSE;
+        } else
+            wait_vbl_done();
+    }
+}
---
+++ src/scene.h
@@ -0,0 +1,93 @@
+#ifndef __SCENE_H__
+//prevents the includes from being called twice
+#define __SCENE_H__
+
+#include <gb/gb.h>
+#include <gb/metasprites.h>
+
+#define MAX_ACTIVE_ACTORS 8
+#define ACTOR_DETECTIVE 0
+#define ACTOR_FIRST_NPC 1
+#define FALL_DELAY 12
+
+#define PLAYER active_actors[ACTOR_DETECTIVE]
+
+#define TO_COORDS(A) ((A) << 4)
+#define TO_PIXELS(A) ((A) >> 4)
+#define TO_TILES(A) ((A) >> (4 + 3))  //4 is "subpixels to pixels" and 3 is "pixels to tiles".
+
+#define GRAVITY 4
+#define FRICTION 2
+#define JUMP_IMPULSE -64
+#define LAUNCH_IMPULSE -94
+#define WALK_VELOCITY 4
+#define MAX_WALK_SPEED 16
+#define MAX_CRAWL_SPEED 10
+#define MAX_FALL_SPEED 64
+//if last_joy and J_A both equal 1, XOR = 0.
+#define CHANGED_BUTTONS (last_joy ^ joy)
+
+typedef enum {
+    DIR_LEFT,
+    DIR_RIGHT,
+    DIR_DOWN_L,
+    DIR_DOWN_R,
+    DIR_CRAWL_L,
+    DIR_CRAWL_R,
+    DIR_IDLE_L,
+    DIR_IDLE_R,
+    DIR_JUMP_L,
+    DIR_JUMP_R,
+} direction_e;
+
+typedef enum {
+    ANIM_LOOP,
+    ANIM_ONCE
+} anim_loop_e;
+
+typedef struct actor_t {
+    UINT16 x;
+    UINT16 y;
+    INT16 SpdX;
+    INT16 SpdY;
+    direction_e direction;
+    direction_e last_direction;
+
+    // tiledata related
+    UINT8 tile_count;
+    UINT8 tile_index;
+    UINT8 frame_delay;
+    const UINT8 *tile_data;
+
+    // animation description
+    const metasprite_t **animations[10];
+    anim_loop_e animations_props[10];
+    UINT8 animation_phase;
+} actor_t;
+
+typedef void (*animate_level_t)();
+
+typedef struct level_t {
+    const actor_t *actors;
+    UINT8 actor_count;
+    animate_level_t animate_hook;
+} level_t;
+
+extern actor_t active_actors[MAX_ACTIVE_ACTORS];
+extern animate_level_t animate_level;
+
+extern UINT8 active_actors_count;
+
+void load_level(const level_t *level);
+void render_actors();
+
+//fuction body is inlined into the code
+inline void SetActorDirection(actor_t *actor, direction_e dir, UBYTE phase) {
+    if (actor->direction != dir) {
+        actor->last_direction = actor->direction;
+        actor->direction = dir;
+        actor->animation_phase = phase;
+    }
+}
+
+#endif
